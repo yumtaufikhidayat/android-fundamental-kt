e70816243f5c56acc8fc75d74cadbd4330d4fc2e
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-25 17:04:53 +0700

    Updating main branch.

--- app/src/main/java/com/taufik/androidfundamental/MainActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.taufik.androidfundamental
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidfundamental.databinding.ActivityMainBinding
-import com.taufik.androidfundamental.debug.DebugActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setActionButton()
-    }
-
-    private fun setActionButton() {
-        binding.apply {
-            btnIntent.setOnClickListener {
-                startActivity(Intent(this@MainActivity, DebugActivity::class.java))
-            }
-        }
     }
 }
