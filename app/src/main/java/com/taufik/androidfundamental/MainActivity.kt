70b82d05ac4491471a4a046fff75ade210ed6613
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-25 18:08:32 +0700

    Adding home fragment.

--- app/src/main/java/com/taufik/androidfundamental/MainActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.taufik.androidfundamental
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidfundamental.databinding.ActivityMainBinding
+import com.taufik.androidfundamental.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        showHomeFragment()
+    }
+
+    private fun showHomeFragment() {
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.i(TAG, "Fragment name: ${HomeFragment::class.java.simpleName}")
+            mFragmentManager.beginTransaction()
+                .add(R.id.frameContainer, mHomeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/taufik/androidfundamental/fragment/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.taufik.androidfundamental.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.taufik.androidfundamental.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: home fragment
+    }
+}
