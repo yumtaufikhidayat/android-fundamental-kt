5b7719197de2ae4d079c21bb1d723a750cb1c522
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-28 15:15:02 +0700

    Adding new fragment.

--- app/src/main/java/com/taufik/androidfundamental/fragment/CategoryFragment.kt
+++ app/src/main/java/com/taufik/androidfundamental/fragment/CategoryFragment.kt
@@ -5,25 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.taufik.androidfundamental.R
-import com.taufik.androidfundamental.databinding.FragmentHomeBinding
+import com.taufik.androidfundamental.databinding.FragmentCategoryBinding
 
-class HomeFragment : Fragment() {
+class CategoryFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // TODO: home fragment
     }
 }
