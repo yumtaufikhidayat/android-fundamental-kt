f43c622910dc6601a0b55304417e98a12bf9dbba
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-02-17 19:10:02 +0700

    Adding custom edit text.

--- app/src/main/java/com/taufik/androidfundamental/activity/CustomButtonEditTextActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/CustomButtonEditTextActivity.kt
@@ -1,6 +1,9 @@
 package com.taufik.androidfundamental.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidfundamental.databinding.ActivityCustomButtonEditTextBinding
 
@@ -12,5 +15,41 @@ class CustomButtonEditTextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomButtonEditTextBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setButtonEnabled()
+    }
+
+    private fun setCustomView() {
+        binding.apply {
+            val result = etCustomEditText.text
+            btnCustomButton.isEnabled = result != null && result.toString().isNotEmpty()
+        }
+    }
+
+    private fun setButtonEnabled() {
+        binding.apply {
+            etCustomEditText.addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // Do nothing
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    setCustomView()
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    // Do nothing
+                }
+            })
+            
+            btnCustomButton.setOnClickListener {
+                Toast.makeText(this@CustomButtonEditTextActivity, etCustomEditText.text, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/taufik/androidfundamental/utils/MyEditText.kt
@@ -0,0 +1,115 @@
+package com.taufik.androidfundamental.utils
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.taufik.androidfundamental.R
+
+class MyEditText: AppCompatEditText, View.OnTouchListener {
+
+    internal lateinit var mClearButtonImage: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        mClearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // DO nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) {
+                    showClearButton()
+                } else {
+                    hideClearButton()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing
+            }
+        })
+    }
+
+    private fun showClearButton() {
+        setCompoundDrawablesWithIntrinsicBounds(null, null, mClearButtonImage, null)
+    }
+
+    private fun hideClearButton() {
+        setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            when (layoutDirection) {
+                View.LAYOUT_DIRECTION_RTL -> {
+                    clearButtonEnd = (mClearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                    when {
+                        event!!.x < clearButtonEnd -> isClearButtonClicked = true
+                    }
+                }
+
+                else -> {
+                    clearButtonStart = (width - paddingEnd - mClearButtonImage.intrinsicWidth).toFloat()
+                    when {
+                        event!!.x > clearButtonStart -> isClearButtonClicked = true
+                    }
+
+                }
+            }
+            when {
+                isClearButtonClicked -> when (event!!.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        mClearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        showClearButton()
+                        return true
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        mClearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+
+                    else -> return false
+                }
+
+                else -> return false
+            }
+        }
+
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        hint = "Masukkan nama Anda"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+}
