4f4f2e12011c9f89cd5c2071d8f2fe328ebcf827
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-19 06:38:34 +0700

    Add option menu.

--- app/src/main/java/com/taufik/androidfundamental/activity/OptionMenuActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/OptionMenuActivity.kt
@@ -1,8 +1,13 @@
 package com.taufik.androidfundamental.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidfundamental.R
 import com.taufik.androidfundamental.databinding.ActivityOptionMenuBinding
+import com.taufik.androidfundamental.fragment.MenuFragment
 
 class OptionMenuActivity : AppCompatActivity() {
 
@@ -12,5 +17,29 @@ class OptionMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOptionMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameContainer, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+
+            R.id.menu2 -> {
+                startActivity(Intent(this, MenuActivity::class.java))
+                true
+            }
+
+            else -> true
+        }
     }
 }
