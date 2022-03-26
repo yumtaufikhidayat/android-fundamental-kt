f55ea46d111274d78d601b7eaa43001fc05a0690
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-26 10:10:09 +0700

    Add nav cart.

--- app/src/main/java/com/taufik/androidfundamental/activity/NavigationDrawerActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/NavigationDrawerActivity.kt
@@ -10,35 +10,56 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.taufik.androidfundamental.R
 import com.taufik.androidfundamental.databinding.ActivityNavigationDrawerBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 class NavigationDrawerActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityNavigationDrawerBinding
+    private lateinit var binding: ActivityNavigationDrawerBinding
+    private lateinit var profileCircleImageView: CircleImageView
+    private var profileImage = "https://lh3.googleusercontent.com/-4qy2DfcXBoE/AAAAAAAAAAI/AAAAAAAABi4/rY-jrtntAi4/s640-il/photo.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityNavigationDrawerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-     binding = ActivityNavigationDrawerBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        setData()
+    }
 
-        setSupportActionBar(binding.appBarNavigationDrawer.toolbar)
+    private fun setData() {
+        binding.apply {
 
-        binding.appBarNavigationDrawer.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            setSupportActionBar(appBarNavigationDrawer.toolbar)
+
+            appBarNavigationDrawer.fab.setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            }
+
+            val drawerLayout: DrawerLayout = drawerLayout
+            val navView: NavigationView = navView
+            val navController = findNavController(R.id.nav_host_fragment_content_navigation_drawer)
+
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            profileCircleImageView = navView.getHeaderView(0).findViewById(R.id.imageView)
+            Glide.with(this@NavigationDrawerActivity)
+                .load(profileImage)
+                .into(profileCircleImageView)
+
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cart
+                ), drawerLayout
+            )
+
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_navigation_drawer)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/taufik/androidfundamental/fragment/CartFragment.kt
@@ -0,0 +1,28 @@
+package com.taufik.androidfundamental.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.taufik.androidfundamental.databinding.FragmentCartBinding
+
+class CartFragment : Fragment() {
+
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
