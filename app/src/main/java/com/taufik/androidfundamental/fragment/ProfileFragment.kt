282e9fbd9be3e87be38fe984fc46790c349b6bb1
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-27 07:04:25 +0700

    Add bottom navigation view.

---
+++ app/src/main/java/com/taufik/androidfundamental/fragment/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.taufik.androidfundamental.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.taufik.androidfundamental.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
