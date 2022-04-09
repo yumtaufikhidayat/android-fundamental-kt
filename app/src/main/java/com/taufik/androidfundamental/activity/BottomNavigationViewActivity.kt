c8cf6eab065285038c9d901c3a03ddfeca68bbbc
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-09 10:24:01 +0700

    Optimize imports project.

--- app/src/main/java/com/taufik/androidfundamental/activity/BottomNavigationViewActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/BottomNavigationViewActivity.kt
@@ -1,12 +1,12 @@
 package com.taufik.androidfundamental.activity
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.taufik.androidfundamental.R
 import com.taufik.androidfundamental.databinding.ActivityBottomNavigationViewBinding
 
--- app/src/main/java/com/taufik/androidfundamental/debug/DebugActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/debug/DebugActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidfundamental.R
-import java.lang.StringBuilder
 
 class DebugActivity : AppCompatActivity(), View.OnClickListener {
 
