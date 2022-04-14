8a72e0fef1ca509d7a26e073d06f1a45955911e2
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-14 15:32:49 +0700

    Update RetrofitActivity.kt by using view binding.

--- app/src/main/java/com/taufik/androidfundamental/activity/RetrofitActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/RetrofitActivity.kt
@@ -1,14 +1,16 @@
 package com.taufik.androidfundamental.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidfundamental.databinding.ActivityRetrofitBinding
 
 class RetrofitActivity : AppCompatActivity() {
 
-    
+    private lateinit var binding: ActivityRetrofitBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_retrofit)
+        binding = ActivityRetrofitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
