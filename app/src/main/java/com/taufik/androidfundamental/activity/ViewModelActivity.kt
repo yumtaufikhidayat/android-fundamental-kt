4b357ce8a7b4020b3df4793c8aeefd81cfb9b8fe
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-02 05:42:38 +0700

    Add ViewModelActivity.

---
+++ app/src/main/java/com/taufik/androidfundamental/activity/ViewModelActivity.kt
@@ -0,0 +1,11 @@
+package com.taufik.androidfundamental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_model)
+    }
+}
