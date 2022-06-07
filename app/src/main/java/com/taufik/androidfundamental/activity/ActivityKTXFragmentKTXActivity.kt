4fc2bc07f0aa69436cc1b082fe33d8e97f7ed824
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-06-07 15:31:20 +0700

    Add activity ktx fragment ktx.

---
+++ app/src/main/java/com/taufik/androidfundamental/activity/ActivityKTXFragmentKTXActivity.kt
@@ -0,0 +1,11 @@
+package com.taufik.androidfundamental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityKTXFragmentKTXActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ktxfragment_ktxactivity)
+    }
+}
