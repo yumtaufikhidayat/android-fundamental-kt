a94dc56daf01974403ae1f0b145978bacb9e90e2
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-08-07 08:33:22 +0700

    Add note sqlite.

---
+++ app/src/main/java/com/taufik/androidfundamental/activity/NoteSQLiteActivity.kt
@@ -0,0 +1,11 @@
+package com.taufik.androidfundamental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteSQLiteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_sqlite)
+    }
+}
