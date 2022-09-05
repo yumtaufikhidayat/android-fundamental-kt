077fbdec9a55373cda9ddcd6e3687d92489b3ac9
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-09-06 06:08:17 +0700

    Implement broadcast receiver with custom event.

--- app/src/main/java/com/taufik/androidfundamental/activity/BroadcastReceiverActivity.kt
+++ app/src/main/java/com/taufik/androidfundamental/activity/BroadcastReceiverActivity.kt
@@ -1,11 +1,18 @@
 package com.taufik.androidfundamental.activity
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidfundamental.databinding.ActivityBroadcastReceiverBinding
+import com.taufik.androidfundamental.permission.PermissionManager
+import com.taufik.androidfundamental.service.DownloadService
 
 class BroadcastReceiverActivity : AppCompatActivity() {
 
@@ -13,27 +20,65 @@ class BroadcastReceiverActivity : AppCompatActivity() {
         ActivityBroadcastReceiverBinding.inflate(layoutInflater)
     }
 
+    private lateinit var downloadReceiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initReceiver()
         setAction()
+    }
+
+    private fun initReceiver() {
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.i(DownloadService.TAG, "onReceive: Download selesai")
+                showToast("Download selesai")
+            }
+        }
+
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     private fun setAction() = with(binding) {
         btnCheckPermission.setOnClickListener {
-            requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+            PermissionManager.check(this@BroadcastReceiverActivity, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
         }
 
+        btnDownload.setOnClickListener {
+            startService(Intent(this@BroadcastReceiverActivity, DownloadService::class.java))
+        }
     }
 
-    private var requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
     ) {
-        if (it) {
-            Toast.makeText(this, "Sms permission receiver diterima", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Sms permission receiver ditolak", Toast.LENGTH_SHORT).show()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            SMS_REQUEST_CODE -> {
+                when (PackageManager.PERMISSION_GRANTED) {
+                    grantResults[0] -> showToast("Sms permission receiver diterima")
+                    else -> showToast("Sms permission receiver ditolak")
+                }
+            }
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(downloadReceiver)
+    }
+
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "com.taufik.androidfundamental.activity.ACTION_DOWNLOAD_STATUS"
+        const val SMS_REQUEST_CODE = 101
     }
 }
---
+++ app/src/main/java/com/taufik/androidfundamental/service/DownloadService.kt
@@ -0,0 +1,32 @@
+package com.taufik.androidfundamental.service
+
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.JobIntentService
+import com.taufik.androidfundamental.activity.BroadcastReceiverActivity
+
+class DownloadService : JobIntentService() {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            enqueueWork(this, this::class.java, BroadcastReceiverActivity.SMS_REQUEST_CODE, intent)
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onHandleWork(intent: Intent) {
+        Log.d(TAG, "Download service dijalankan")
+        try {
+            Thread.sleep(5000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        val notifyFinishIntent = Intent(BroadcastReceiverActivity.ACTION_DOWNLOAD_STATUS)
+        sendBroadcast(notifyFinishIntent)
+    }
+
+    companion object {
+        val TAG = DownloadService::class.java.simpleName
+    }
+}
